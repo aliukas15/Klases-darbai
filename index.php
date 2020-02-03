3b43ba219830fe70afe5f2dc6c67b50dff1da422
aliukas15 <audry6@gmail.com>
2020-02-03 11:17:17 +0200

    Kom

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
 
-print 'veikia';
+//"1. ekrane atvaizduokite teksta 'Sios dienos data - ' ir su php atvaizduokite sios dienos data"
 
+//"2. ekrane atvaizduokite sakini is 5<zodziu. Po kiekvie"
+$string = 'Sios dienos data - ';
+$data = date('Y/m/d');
+
+$result = $string . $data;
+
+print $result;
