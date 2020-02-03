e1e7386d715045cf0cae3265e61257c8a67c9490
aliukas15 <audry6@gmail.com>
2020-02-03 15:02:24 +0200

    Tpppp

--- index.php
+++ index.php
@@ -1,43 +1,31 @@
 <?php
 
-//"1. ekrane atvaizduokite teksta 'Sios dienos data - ' ir su php atvaizduokite sios dienos data"
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+// Brendas, Modelis, Metai, 
+$cars =
+[
+	[
+		'brand' => 'audi',
+		'model' => 'a4',
+		'year' => 2002,
+		'cost' => 22000,
+	],
+	[
+		'brand' => 'bmw',
+		'model' => 'e35',
+		'year' => 2005,
+		'cost' => 27000,
+	],
+	[
+		'brand' => 'zigulys',
+		'model' => '8',
+		'year' => 1984,
+		'cost' => 12000,
+	],
 
-//"2. ekrane atvaizduokite sakini is 5<zodziu. Po kiekvie"
-//$string = 'Sios dienos data - ';
-//$data = date('Y/m/d');
-
-//$result = $string . $data;
-
-//print $result;
-//"2. sukurti sakini is 5< zodziu. po kiekvieno zodzio yra padetas taskas. jums reikia naudoti bazines php funkcijas tuos taskus pakeisti i tarpus."
-
-
-//"3. Sukurti stringa kuris susidarytu is sakinio. Turi jame egzistuoti didziosios ir mazosios raides. Panaudoti php default'ine funkcija kuri visas stringo raides paverstu didziosiomis arba mazosiomis"
-$targetUp = 'Pirmadienis';
-
-switch (strtoupper($targetUp)) {
-	case 'PIRMADIENIS':
-		PRINT 'Pirmadienis';
-		break;
-	case 'ANTRADIENIS':
-		PRINT 'Antradienis';
-		break;
-	case 'TRECIADIENIS':
-		PRINT 'Treciadienis';
-		break;
-	case 'KETVIRTADIENIS':
-		PRINT 'Ketvirtadienis';
-		break;
-	case 'PENKTADIENIS':
-		PRINT 'Penktadienis';
-		break;
-	case 'SESTADIENIS':
-		PRINT 'Sestadienis';
-		break;
-	case 'SEKMADIENIS':
-		PRINT 'Sekmadienis';
-		break;
-default:
-	print 'Nera tokios savaites dienos';
-	break;
+];
+foreach ($cars as $key => $value) {
+var_dump($value['brand']);
 }
